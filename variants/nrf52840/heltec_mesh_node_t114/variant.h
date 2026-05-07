92b683e4e73a15d35429b8403c785accdd8b7f0b
jackyes <jackyes@users.noreply.github.com>
2026-05-07 16:00:20 +0200

    Update variant.h

--- variants/nrf52840/heltec_mesh_node_t114/variant.h
+++ variants/nrf52840/heltec_mesh_node_t114/variant.h
@@ -28,7 +28,7 @@
 #define HAS_TRAFFIC_MANAGEMENT 1
 #endif
 #ifndef TRAFFIC_MANAGEMENT_CACHE_SIZE
-#define TRAFFIC_MANAGEMENT_CACHE_SIZE 2048
+#define TRAFFIC_MANAGEMENT_CACHE_SIZE 1024
 #endif
 
 /*----------------------------------------------------------------------------
