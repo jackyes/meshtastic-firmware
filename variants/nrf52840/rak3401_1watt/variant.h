096b69a883e50314572f537782f2c0ac25ec3a07
jacky <jacky@meshtastic.local>
2026-07-07 16:44:34 +0200

    Fix nRF52 TRAFFIC_MANAGEMENT_CACHE_SIZE to fit MEM_CLASS_SMALL budget
    Five nRF52840 variants had TRAFFIC_MANAGEMENT_CACHE_SIZE=1024 which
    exceeded the 16 KB MEM_CLASS_SMALL boot cache budget (19040 > 16384).
    Reduced to 750 (16300, within budget). Affected:
    - rak3401_1watt
    - heltec_mesh_node_t114
    - rak4631
    - seeed_xiao_nrf52840_kit
    - seeed_wio_tracker_L1

--- variants/nrf52840/rak3401_1watt/variant.h
+++ variants/nrf52840/rak3401_1watt/variant.h
@@ -32,7 +32,7 @@
 #define HAS_TRAFFIC_MANAGEMENT 1
 #endif
 #ifndef TRAFFIC_MANAGEMENT_CACHE_SIZE
-#define TRAFFIC_MANAGEMENT_CACHE_SIZE 1024
+#define TRAFFIC_MANAGEMENT_CACHE_SIZE 750
 #endif
 
 /*----------------------------------------------------------------------------
